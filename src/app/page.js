d713c8d2be9366a27b203e57427c857b3b1dea66
Steen Lillethorup Frederiksen <slf@dbc.dk>
2024-11-19 15:19:44 +0100

    First commit

---
+++ src/app/page.js
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <Image
+          className={styles.logo}
+          src="/Vanlife Tracker Logo.svg"
+          alt="Van"
+          width={300}
+          height={300}
+          priority
+        />
+
+      </main>
+      <footer className={styles.footer}>
+        <a
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a>
+      </footer>
+    </div>
+  );
+}
